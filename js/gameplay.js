7f53362aa46e2356108c55d74155648a525683ef
Wadson Pontes <wadsonpontesdesouza@hotmail.com>
2022-11-29 12:00:33 -0300

    Classes da gamemplay (em desenvolvimento)

--- js/gameplay.js
+++ js/gameplay.js
@@ -19,23 +19,80 @@
 // 	});
 // }
 
+const HABILIDADES = [
+	['id', 'nome_habilidade', 'descricao', 'imagem',                ],
+	[1,    'Cura',            '',          'img/icon/skill/cura.png'],
+];
+
+const ESPECIAIS = [
+	['id', 'nome_especial', 'descricao', 'imagem',                       ],
+	[1,    'Furtividade',   '',          'img/icon/skill/furtividade.png'],
+];
+
+const TIPOS_DE_CARTAS = [
+	['id', 'nome_tipo', 'descricao', 'imagem',                       ],
+];
+
+const CARTAS = [
+	['id', 'nome_carta',    'descricao', 'imagem',         'id_tipo', 'nivel', 'dano', 'saude', 'id_primaria', 'id_secundaria', 'id_terciaria', 'id_especial'],
+	[1,    'Dragão Branco', '',          'img/card/1.jpg', 1,         'nivel', 'dano', 'saude', 'id_primaria', 'id_secundaria', 'id_terciaria', 'id_especial'],
+];
+
+class Habilidade {
+	constructor(id, valor) {
+		this.id = id;
+		this.nome_habilidade = HABILIDADES[id].nome_habilidade;
+		this.descricao = HABILIDADES[id].descricao;
+		this.imagem = HABILIDADES[id].imagem;
+		this.valor_base = valor;
+		this.valor = valor;
+	}
+}
+
+class Especial {
+	constructor(id) {
+		this.id = id;
+		this.nome_especial = ESPECIAIS[id].nome_especial;
+		this.descricao = ESPECIAIS[id].descricao;
+		this.imagem = ESPECIAIS[id].imagem;
+	}
+}
+
 class Carta {
-	constructor(id, nome_carta, descricao, tipo, elemento, nivel, dano, saude) {
+	constructor(id, nome_carta, descricao, imagem, tipo, elemento, signo, nivel, dano, saude) {
 		this.id = id;
 		this.nome_carta = nome_carta;
 		this.descricao = descricao;
+		this.imagem = imagem;
 		this.tipo = tipo;
 		this.elemento = elemento;
+		this.signo = signo;
 		this.nivel = nivel;
+		this.dano_base = dano;
+		this.saude_base = saude;
 		this.dano = dano;
 		this.saude = saude;
+		this.primaria = Habilidade(id, 1);
+		this.secundaria = Habilidade(id, 2);
+		this.terciaria = Habilidade(id, 3);
+		this.especial = new Especial();
 	}
 }
 
 class Personagem {
-	constructor(id) {
-		this.nome_jogador = sessionStorage.getItem(`nome_jogador_${id}`);
-		this.personagem = new this.personagem(); sessionStorage.getItem(`personagem_${id}`);
+	constructor(id, nome_personagem, descricao, imagem, classe, nivel, dano, saude) {
+		this.id = id;
+		this.nome_personagem = nome_personagem;
+		this.descricao = descricao;
+		this.imagem = imagem;
+		this.classe = classe;
+		this.nivel = nivel;
+		this.dano = dano;
+		this.saude = saude;
+		this.primaria = Habilidade(id, 1);
+		this.secundaria = Habilidade(id, 2);
+		this.terciaria = Habilidade(id, 3);
+		this.especial = new Especial();
 	}
 }
 

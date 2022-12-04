80e182ae70737afbe4423079e24b5a9d278f4136
rbert1n <robert.targino@hotmail.com>
2022-12-03 23:37:40 -0300

    Validação e-mail e materialize.min.js

--- php/register.service.php
+++ php/register.service.php
@@ -3,27 +3,38 @@
 require_once('api.php');
 
 function validar_cadastro() {
+
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $nome_usuario = $_POST['nome_usuario'];
+
     $resposta = array(
         'status' => 'error',
         'error' => ''
     );
 
-    if (!isset($_POST['nome_usuario']) || !isset($_POST['email']) || !isset($_POST['senha'])) {
+    if (!isset($nome_usuario) || !isset($email) || !isset($senha)) {
         echo json_encode($resposta);
         exit();
     }
 
-    if (strlen(trim($_POST['nome_usuario'])) < 3) {
+    if (strlen(trim($nome_usuario)) < 3) {
         echo json_encode($resposta);
         exit();
     }
 
-    if (strlen(trim($_POST['email'])) < 3) {
+    if (strlen(trim($email)) < 3) {
         echo json_encode($resposta);
         exit();
     }
 
-    if (strlen(trim($_POST['senha'])) < 3) {
+    if (strlen(trim($senha)) < 3) {
+        echo json_encode($resposta);
+        exit();
+    }
+
+    if (existe('jogador', 'email', $email)){
+        $resposta['error']= 'E-mail já cadastrado';
         echo json_encode($resposta);
         exit();
     }

c31294d3321564ac7e4e87ff6fc568b5c2128973
Wadson Pontes <wadsonpontesdesouza@hotmail.com>
2022-12-05 10:23:31 -0300

    Função/Animação puxar (em andamento)

--- js/carta.gameplay.js
+++ js/carta.gameplay.js
@@ -17,32 +17,46 @@ class Carta {
 		this.especial = new Especial(CARTAS[id][13]);
 	}
 
-  async animPuxar(carta, i) {
-    this.setarCartaCompra();
-
-    await dormir(1000);
-
-    jogador.el_deck.style.transition = '';
-    jogador.at_deck.textContent = '';
-    jogador.vi_deck.textContent = '';
-    jogador.at_deck.style.display = 'none';
-    jogador.vi_deck.style.display = 'none';
-    jogador.el_deck.style.backgroundImage = '';
-    jogador.el_deck.style.zIndex = '-1';
-
-    jogador.el_deck.style.transform ="translate(0, 0)";
-
-    await dormir(500);
-
-    t['mao' + jogador.id][i].card = carta;
-    t['mao' + jogador.id][i].at.textContent = carta.ataque;
-    t['mao' + jogador.id][i].vi.textContent = carta.vida;
-    t['mao' + jogador.id][i].at.style.display = 'block';
-    t['mao' + jogador.id][i].vi.style.display = 'block';
-    t['mao' + jogador.id][i].el.style.backgroundImage = 'url("'+ carta.img +'")';
+  async animPuxar(i) {
+    this.mostrarCompra(i);
+    await this.moverCompra(i);
+    this.esconderCompra();
+    await this.voltarCompra(i);
+    this.mostrarCarta(i, 'mao');
   }
 
-	setarCartaCompra() {
+  async moverCompra(i) {
+    let x = parseInt(this.jogador.el.mao[i].style.left);
+    let y = 0;
+
+    this.jogador.el.compra.style.transform =`translate(${(420 + 200 * i)}px, ${y}px)`;
+    await dormir(1000);
+  }
+
+  async voltarCompra(i) {
+    this.jogador.el.compra.style.transform ="translate(0, 0)";
+    await dormir(500);
+  }
+
+  mostrarCarta(local, i) {
+    this.jogador.el[local][i].imagem.src = this.imagem;
+    this.jogador.el[local][i].nome.textContent = this.nome_carta;
+    this.jogador.el[local][i].nivel.textContent = this.nivel;
+    this.jogador.el[local][i].tipo.src = this.tipo.imagem;
+    this.jogador.el[local][i].dano.textContent = this.dano;
+    this.jogador.el[local][i].saude.textContent = this.saude;
+    this.jogador.el[local][i].primaria.valor.textContent = this.primaria.valor;
+    this.jogador.el[local][i].primaria.icone.src = this.primaria.imagem;
+    this.jogador.el[local][i].secundaria.valor.textContent = this.secundaria.valor;
+    this.jogador.el[local][i].secundaria.icone.src = this.secundaria.imagem;
+    this.jogador.el[local][i].terciaria.valor.textContent = this.terciaria.valor;
+    this.jogador.el[local][i].terciaria.icone.src = this.terciaria.imagem;
+    this.jogador.el[local][i].especial.src = this.especial.imagem;
+    this.jogador.el[local][i].el.style.visibility = 'visible';
+    this.jogador.el[local][i].style.transition = 'all 1.0s linear';
+	}
+
+	mostrarCompra() {
     this.jogador.el.compra.imagem.src = this.imagem;
     this.jogador.el.compra.nome.textContent = this.nome_carta;
     this.jogador.el.compra.nivel.textContent = this.nivel;
@@ -56,21 +70,12 @@ class Carta {
     this.jogador.el.compra.terciaria.valor.textContent = this.terciaria.valor;
     this.jogador.el.compra.terciaria.icone.src = this.terciaria.imagem;
     this.jogador.el.compra.especial.src = this.especial.imagem;
-
-		this.jogador.el.compra.style.transition = 'all 1.0s linear';
-		
-		jogador.at_deck.textContent = carta.ataque;
-		jogador.vi_deck.textContent = carta.vida;
-		jogador.at_deck.style.display = 'block';
-		jogador.vi_deck.style.display = 'block';
-		jogador.el_deck.style.backgroundImage = 'url("'+ carta.img +'")';
-		jogador.el_deck.style.zIndex = '5';
-	
-		if (jogador.id == 1) {
-		  jogador.el_deck.style.transform ="translate(" + (420 + 200 * i) + "px, -20px)";
-		}
-		else {
-		  jogador.el_deck.style.transform = "translate(" + (420 + 200 * i) + "px, 20px)";
-		}
+    this.jogador.el.compra.el.style.visibility = 'visible';
+    this.jogador.el.compra.style.transition = 'all 1.0s linear';
 	}
+
+  esconderCompra() {
+    this.jogador.el.compra.el.style.visibility = 'hidden';
+    this.jogador.el.compra.style.transition = '';
+  }
 }
